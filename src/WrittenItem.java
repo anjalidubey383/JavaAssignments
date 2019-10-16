2e3296404c6a7324de88db07995f63e7bad77082
temp <temp@DIN34003360.corp.capgemini.com>
2019-10-16 15:21:47 +0530

    Added new Project

---
+++ src/WrittenItem.java
@@ -0,0 +1,10 @@
+
+public abstract class WrittenItem extends Items{
+ private String author;
+	public WrittenItem(int idNum, String title, int noOfCopies) {
+		super(idNum, title, noOfCopies);
+	
+	}
+	public abstract void addItem();
+	
+}
